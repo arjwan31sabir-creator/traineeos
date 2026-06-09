2dc5aab96751abb07c3846d4b10c03a0267ef7c9
Arjwan Albalushi <aWX1482580@china.huawei.com>
2026-06-09 18:40:27 +0400

    Fix Supabase anon key - access log will now work

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://kbfttuwblanvkiocgktf.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_vQIUaiCdIGeOx9yXQHUScQ_N7itSU9Q'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtiZnR0dXdibGFudmtpb2Nna3RmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3NTYxMDEsImV4cCI6MjA5NjMzMjEwMX0.-lCHjSm674e0ePV8fMZmwDrX_XFulxjNy7vA0VC_fqE'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
